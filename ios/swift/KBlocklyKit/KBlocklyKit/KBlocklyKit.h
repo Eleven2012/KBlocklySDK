ca6f8b2f473fe62516643657d46f2b4b51cea300
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 14:36:51 +0800

    init project

---
+++ ios/swift/KBlocklyKit/KBlocklyKit/KBlocklyKit.h
@@ -0,0 +1,19 @@
+//
+//  KBlocklyKit.h
+//  KBlocklyKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KBlocklyKit.
+FOUNDATION_EXPORT double KBlocklyKitVersionNumber;
+
+//! Project version string for KBlocklyKit.
+FOUNDATION_EXPORT const unsigned char KBlocklyKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KBlocklyKit/PublicHeader.h>
+
+
